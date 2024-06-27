accabbd2f4a71668b9711414081368d646cc9292
Fran Pacheco <pachecofrancisco1999@gmail.com>
2024-06-27 18:23:18 -0300

    se modifico y corrigio logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <header className="header">
     <Link to="/" className="link-titulo">
-      <img src="src\assets\logo.jpg" alt="logo" className="logo-img" />
+      <img src="https://firebasestorage.googleapis.com/v0/b/tienda-reactjs-coderhouse24.appspot.com/o/logo%20y%20varios%2Flogo.jpg?alt=media&token=baf2bf94-5999-4b2d-ae2d-baac91075a48" alt="logo" className="logo-img" />
     </Link>
     <NavBar/>
     <CartWidget />
